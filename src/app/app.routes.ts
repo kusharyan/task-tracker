98cc5b45e320b25b7c9bf8a8a0f3b08fdc589c72
kusharyan <pilliaryan4@gmail.com>
2025-05-30 19:15:41 +0530

    Minor changes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,21 +4,21 @@ import { TaskItemComponent } from './components/task-item/task-item.component';
 import { TaskListComponent } from './components/task-list/task-list.component';
 
 export const routes: Routes = [
-  {
-    path: 'add-tasks',
-    component: AddTasksComponent
-  },
-  {
-    path: 'task-item',
-    component: TaskItemComponent
-  }, 
-  {
-    path: 'task-list',
-    component: TaskListComponent
-  },
-  {
-    path: '',
-    redirectTo: 'add-tasks',
-    pathMatch: 'full'
-  }
+  // {
+  //   path: 'add-tasks',
+  //   component: AddTasksComponent
+  // },
+  // {
+  //   path: 'task-item',
+  //   component: TaskItemComponent
+  // }, 
+  // {
+  //   path: 'task-list',
+  //   component: TaskListComponent
+  // },
+  // {
+  //   path: '',
+  //   redirectTo: 'add-tasks',
+  //   pathMatch: 'full'
+  // }
 ];
